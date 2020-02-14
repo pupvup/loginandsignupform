a51c38fcbb8cedda7ee95ab9abaf49237e99f1a0
pupvup <irusya14@gmail.com>
2020-02-14 18:00:13 +0300

    last commit

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+            
+        $link = mysqli_connect("localhost", "id12531403_plumdiary", "plumdiary", "id12531403_diary");
+        if (mysqli_connect_error())
+            die("Can't connect to Data base.");
+        
+?>
---
+++ header.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <title>Secret Diary!</title>
+        <style type="text/css">
+            .container {
+                text-align: center;
+                width: 400px;
+                background-color:rgba(0, 0, 0, 0.5);
+            }
+            .container-fluid {
+                text-align: center;
+                
+            }
+            #login-container {
+                margin-top: 70px;
+            }
+            #home-container {
+                margin-top: 270px;
+            }
+            html {
+                background: url(bg.jpg) no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+            body {
+                padding-bottom: 20px;
+                background: none;
+            }
+            #login-form {
+                display: none;
+            }
+            h1 {
+                padding-top: 20px;
+            }
+            #note {
+                width: 100%;
+                height: 100%;
+            }
+            textarea {
+                margin-top: 10px;
+            }
+        </style>
+    </head>
